dbd464c4d53371741ad066187b252caf99e69873
Chandan ray <123835074+amichandan@users.noreply.github.com>
2023-09-29 11:07:04 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,26 @@
+// different datatypes
+
+
+// number => 2 to the power 53
+// for bigger number we can use => bigint
+// string => "" , ''
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => for uniqueness
+
+
+// object
+
+
+let myAge = 25
+let myName = "chandan"
+let isLoggedIn = false
+let state;
+let myResult = null
+
+
+console.log(typeof 25) // to find the type of any data
+console.table([ myAge , myName , isLoggedIn , state , myResult ])
+console.log(typeof null)   //  result = object
+console.log(typeof undefined)  // undefined
