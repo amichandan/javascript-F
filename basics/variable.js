bc04fcac329a1d941bc5c7c4cb6f601a9415f274
Chandan ray <123835074+amichandan@users.noreply.github.com>
2023-09-29 03:46:04 +0000

    variables

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 1234
+let accountEmail = "chandanray@gmail.com"
+var accountPassword = "1234"
+accountCity = "jamshedpur"
+let accountState;
+
+console.log(accountId)
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+
