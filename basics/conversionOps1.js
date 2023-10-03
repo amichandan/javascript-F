62f556ca3b88c3d53880a389cce890ff78e3d8de
Chandan ray <123835074+amichandan@users.noreply.github.com>
2023-10-03 12:13:26 +0000

    convervion operations part2

---
+++ basics/conversionOps1.js
@@ -0,0 +1,39 @@
+// **************OPERATIONS********************//
+
+// some basic operations
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)       // 2 to the power 3
+
+
+// other operations 
+
+let value = 5
+let negValue = -value
+console.log(negValue);     // -5
+
+let str1 = "hii"
+let str2 = " chandan"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 3);
+console.log(1 + "3");
+console.log("3" + 2 + 2);
+console.log(3 + 2 + "2");
+console.log( (3 + 2) * 4 + "2");
+
+console.log(true);      // true
+console.log(+true);      // 1   
+console.log(+"");        // 0
+
+let counting = 20
+counting++;
+console.log(counting);
+
+
+
+// there are so many operator in javascript
